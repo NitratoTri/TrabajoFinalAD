386ba83e08c145b14dbc6fd05bd3e4863cc2fc6b
NitratoTri <148335354+NitratoTri@users.noreply.github.com>
2025-02-10 16:52:41 +0100

    Added folder for bootstrap carousel imgs in index

--- src/main/java/com/example/FrikadasVarias/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.FrikadasVarias.repository;
-
-import com.example.FrikadasVarias.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- src/main/java/com/example/FrikadasVarias/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.FrikadasVarias.repository;
-
-
-import com.example.FrikadasVarias.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
