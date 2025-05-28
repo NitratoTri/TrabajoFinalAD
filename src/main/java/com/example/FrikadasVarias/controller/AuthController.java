785461210b555f358a5cbe1a1065ceb6be0c635a
pablo <pabloalvarezolaya@gmail.com>
2025-05-28 13:17:55 +0200

    Add email confirmation on user registration

--- src/main/java/com/example/FrikadasVarias/controller/AuthController.java
+++ src/main/java/com/example/FrikadasVarias/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.example.FrikadasVarias.controller;
 
 import com.example.FrikadasVarias.dto.UserDto;
 import com.example.FrikadasVarias.entity.User;
+import com.example.FrikadasVarias.service.EmailService;
 import com.example.FrikadasVarias.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,6 +20,8 @@ import java.util.List;
 public class AuthController {
 
     private UserService userService;
+    @Autowired
+    private EmailService emailService;
 
     public AuthController(UserService userService) {
         this.userService = userService;
@@ -38,6 +42,15 @@ public class AuthController {
             return "register";
         }
         userService.saveUser(user);
+        //Send a confirmation email
+        String emailContent = "<h1>Bienvenido a FrikadasVarias!</h1>" +
+                "<p>Gracias por registarte y espero que disfrute de la app!</p>" +
+                "<p>Username: " + user.getEmail() + "</p>";
+        emailService.enviarCorreo(
+                user.getEmail(),
+                "Bienvenido a Frikadas Varias",
+                emailContent
+        );
         return "redirect:/register?success";
     }
 
